1026b1db721fc33ffd94d737119bf1258e8ec578
Asman <antonisasm99@gmail.com>
2025-07-14 17:12:21 +0300

    Changed the first step(DATA PREPROCESSING AND EDA)

--- ml.py
+++ ml.py
@@ -10,11 +10,12 @@ from sklearn import linear_model
 from sklearn.feature_selection import SelectKBest , f_classif
 from sklearn.decomposition import PCA
 from matplotlib.colors import ListedColormap
+from sklearn.metrics import mean_absolute_error
 import seaborn as sns
 import pandas as pd
 import random
 
-###################################### DATA LOAD AND PREPARATION/PREPROCESSING #################################################
+###################################### DATA LOAD/PREPARATION/EDA ###############################################################
 SEED=np.random.randint(0,10e7)
 print(f"SEED: {SEED}")
 random_state = SEED
@@ -37,21 +38,11 @@ print(df.shape)
 #X-> all the features , y ->classes column
 X = df.iloc[: , 1:]
 y = df.iloc[: , 0]
+
 #set the problem into a binary classification problem by putting M=1 and B=0
 le = LabelEncoder()
 y=le.fit_transform(y)
 
-
-#Split the X and y into train and test subsets for furhter analysis
-X_train , X_test , y_train , y_test = train_test_split(X ,y , test_size=0.2 , train_size=0.8  ,random_state=random_state)
-
-#I will use the KNN classifier which is based on distances between data points.So the values need to be scaled(only for the KNN)
-scl = StandardScaler()
-X_train_scaled = scl.fit_transform(X_train)
-X_test_scaled = scl.transform(X_test)
-
-
-################################### EXPLORATORY DATA ANALYSIS ############################################################################
 #Histograms of all the features
 labels = []
 for label in X.columns:
@@ -69,47 +60,44 @@ def plot_hist(X , bins):
     plt.show()
 plot_hist(X,30)
 
+#Plot boxplots for each feature
 
+def box_plot(X):
+    fig , axes = plt.subplots(5,6 , figsize = (15,15))
+    axes = axes.flatten()
+    for i, label in enumerate(labels):
+        axes[i].boxplot(X[label] ,orientation = 'vertical' , notch = False )
+        axes[i].set_title(f"{label}")
+    plt.tight_layout()
+    plt.show()
+box_plot(X)
+
+    
 #Correlation Heatmap for all the features of the dataset
-
-X_train_scaled_data = pd.DataFrame(X_train_scaled , columns=X.columns) #transform X_train_scaled into a dataframe to use corr func
-p_corr = X_train_scaled_data.corr()
+p_corr = X.corr()
 
 plt.figure(figsize=(8,8))
 sns.heatmap(p_corr ,cmap= 'crest' , linewidths=0.1 , linecolor='b' , xticklabels=True , yticklabels=True)
 plt.title("Correlation heatmap for the dataset features")
 plt.show()
 
+
+#Split the X and y into train and test subsets for furhter analysis
+X_train , X_test , y_train , y_test = train_test_split(X ,y , test_size=0.2 , train_size=0.8  ,random_state=random_state)
+
+#I will use the KNN classifier which is based on distances between data points.So the values need to be scaled(only for the KNN)
+scl = StandardScaler()
+X_train_scaled = scl.fit_transform(X_train)
+X_test_scaled = scl.transform(X_test)
+
+
+
 ##################################### DATA ANALYSIS/MODELING ##################################################################
 
+#On this part i will use different models to see which one has the best accuracy for this dataset
 #KNN classifier 
+
 #Form a pipeline (feature selection inside GridSearch)
 
-params ={'feature_selection__k': [10, 15, 20,25,30],  # How many features to select
-    'classifier__n_neighbors': [10, 15, 20, 25, 30],          # KNN neighbors
-    'classifier__weights': ['uniform', 'distance'],            # KNN weights
-    'classifier__metric': ['minkowski'],                       # KNN metric
-    'classifier__p': [1, 2]}                                    # p=1-> ManHattan , p=2->Euclidean
 
-#Pipeline of feature selection for the KNN classifier
-pipeline = Pipeline([('feature_selection' , SelectKBest(score_func=f_classif)) , ('classifier' ,KNeighborsClassifier())])
-
-#Gridsearch to slect the best features and best hyperparams for the model
-grid_search = GridSearchCV(pipeline , param_grid=params , cv = KFold(n_splits=5 , shuffle=True,random_state=random_state) , scoring='accuracy' , verbose=1)
-grid_search.fit(X_train_scaled , y_train)
-grid_pred = grid_search.predict(X_test_scaled)
-
-
-print("Best parameters:", grid_search.best_params_)
-print("Best CV score:", grid_search.best_score_)
-
-#Get the selected features
-best_model = grid_search.best_estimator_
-feature_selector = best_model.named_steps['feature_selection']
-selected_features = X.columns[feature_selector.get_support()]
-print("Selected features:", list(selected_features))
-
-# Get classifier parameters:
-classifier = best_model.named_steps['classifier'] 
-print(f"KNN: n_neighbors={classifier.n_neighbors}, weights={classifier.weights} , metric:{classifier.metric} , p:{classifier.p}")
 
